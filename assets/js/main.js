e8e9f129bc64a579da549f4a5dbf5fde9b3618dd
fiyo-19 <fiyoagatha98@gmail.com>
2024-10-08 10:32:35 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// js/main.js
+import { initSwipers } from './modules/swiperConfig.js';
+import './global/aosConfig.js';
+
+// Inisialisasi semua Swiper di halaman
+document.addEventListener('DOMContentLoaded', function() {
+    initSwipers();
+});
